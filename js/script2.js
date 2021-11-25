343a55b5145126be59adcdc085867d4410e65f01
John Pensotes <johnfernando.pensotes.iics@ust.edu.ph>
2021-11-25 13:40:45 +0800

    added script2.js

--- js/script2.js
+++ js/script2.js
@@ -1,10 +1,12 @@
-function isOdd(A) {
-    if (A % 2==1) {
-        return true;
-    } else {
-      return false;
+var a = [50]
+
+function isOdd(){
+    if (a%2 === 1){
+        return true
+    }else{
+        return false
     }
 }
 
-console.log(isOdd(500));
+console.log(isOdd(a))
 
